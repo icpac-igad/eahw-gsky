daac031addbc5a631ebceaca0c8faebe6bb2da98
Erick Otenyo <otenyo.erick@gmail.com>
2021-11-14 12:12:03 +0300

    Add memcached for caching MAS requests

--- gsky/ingest_data.sh
+++ gsky/ingest_data.sh
@@ -13,11 +13,11 @@ export LD_LIBRARY_PATH="/usr/local/lib:${LD_LIBRARY_PATH:-}"
 
 export PGUSER=postgres
 export PGDATA=/pg_data
+MEMCACHE_DEV_TCP_FILE=${MEMCACHE_DEV_TCP_FILE:=-""}
 
 set +x
 res=$(find "$CRAWL_DIR" -name "${CRAWL_PATTERN}")
-if [ -z "$res" ]
-then
+if [ -z "$res" ]; then
   echo "No ${CRAWL_PATTERN} files under '$CRAWL_DIR'."
 else
   set -x
@@ -30,4 +30,9 @@ else
   crawl_job_id="${CRAWL_DIR//[\/]/_}"
 
   (cd /gsky/share/mas && ./ingest_pipeline.sh $SHARD_NAME /crawl_outputs/${crawl_job_id}_gdal.tsv.gz)
+
+  # flush memcache on each ingestion
+  if [ ! -z "$MEMCACHE_DEV_TCP_FILE" ]; then
+    echo flush_all > /dev/tcp/$MEMCACHE_DEV_TCP_FILE
+  fi
 fi
